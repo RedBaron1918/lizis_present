76499662b7a2aaab01810fef5d8af2e302680742
RedBaron1918 <asatianikotiko1@gmail.com>
2025-01-31 12:41:59 +0400

    fixed animations

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
-
 function random(num) {
     return Math.floor(Math.random() * num);
 }
+
+
 
 function getRandomStyles() {
     var r = random(255);
@@ -15,8 +15,17 @@ function getRandomStyles() {
         color: rgba(${r},${g},${b},0.7); 
         box-shadow: inset -7px -3px 10px rgba(${r - 10},${g - 10},${b - 10},0.6);
         left: ${left}px;
-      animation: float ${dur}s ease-in 1; 
+        animation: float ${dur}s ease-in 1; 
     `;
+}
+
+function getNameStyle() {
+    return `
+         background-color: rgba(${200},${200},${200},0.7);
+         color: #000;
+        animation: test 5s ease-in 1 forwards; 
+
+    `
 }
 
 function createBalloons(num) {
@@ -27,15 +36,26 @@ function createBalloons(num) {
         balloon.style.cssText = getRandomStyles();
         document.body.append(balloon);
         balloons.push(balloon)
-
     }
-
     setTimeout(() => {
         balloons.forEach(balloon => {
             balloon.remove()
         })
 
     }, 5000)
+}
+
+function createName() {
+    const name = document.querySelector(".name");
+    const lizi = 'LIZI';
+
+    for (let i = 0; i < lizi.length; i++) {
+        var balloon = document.createElement("div");
+        balloon.className = "balloon";
+        balloon.style.cssText = getNameStyle();
+        balloon.textContent = lizi[i];
+        name.appendChild(balloon);
+    }
 }
 
 const buttonFunctions = {
@@ -54,27 +74,48 @@ const buttonFunctions = {
             document.querySelectorAll(".bulb").forEach(function (bulb) {
                 const img = bulb.querySelector("img");
                 img.src = "bulb_orange.png";
-                img.alt = "bulb";
                 img.height = 50;
                 img.width = 50;
                 bulb.appendChild(img);
             });
         }, 1000);
 
-        updateButton('Get Decorations');
+        updateButton('Put On Some Decorations');
     },
-    'Get Decorations': function () {
+    'Put On Some Decorations': function () {
 
         const banner = document.querySelector(".banner");
         banner.classList.add("drop-banner");
-        updateButton('Get Baloons');
+        updateButton('LET THERE BE BALLOONS');
     },
-    'Get Baloons': function () {
-        console.log("Baloons");
+    'LET THERE BE BALLOONS': function () {
+        console.log("BalLoons");
         createBalloons(30)
-        updateButton('Get Decorations');
+        createName();
+        updateButton('and finally STRAWBERRY CAKE');
+    },
+
+    'and finally STRAWBERRY CAKE': function () {
+        const cake = document.querySelector(".cake-wrapper");
+        cake.style.cssText = `
+         animation: cake 3s ease-in 1 forwards; 
+        `;
+        const text = document.querySelector(".text");
+        text.textContent = "Happy Birthday Special Girl ❤";
+        text.style.cssText = `
+         animation: cake 4s ease-in 1 forwards; 
+        `;
+        const btn = document.querySelector('.btn');
+        btn.style.cssText = `
+         animation: fadeOut 1s forwards; 
+        `;
+        setTimeout(() => {
+            btn.remove();
+        }, 1500);
 
     }
+
+
 };
 
 function updateButton(nextFunction, fadeAway = true) {
@@ -87,8 +128,11 @@ function updateButton(nextFunction, fadeAway = true) {
         }, 3000);
     }
 
-    button.querySelector("span").textContent = nextFunction;
-    button.onclick = buttonFunctions[nextFunction];
+    setTimeout(() => {
+        button.querySelector("span").textContent = nextFunction;
+        button.onclick = buttonFunctions[nextFunction];
+    }, 500)
+
 }
 
 
